7e8f65fa7a6583448596b1690913ef127324581d
CoenNaude <cnaude667@gmail.com>
2019-01-31 16:48:52 +0200

    confirmation page

--- confirm.php
+++ confirm.php
@@ -1,2 +1,32 @@
 <p> you have successfully registered </p>
 <a href="index.php">Log in</a>
+
+<?php
+
+echo 'Hello bra';
+    if(isset($_GET['username']) && isset($_GET['code'])){
+    
+    
+    try{
+    $con = new PDO ("mysql:host=localhost;dbname=camagru","root","123456");
+    {
+        $email = $_GET['username'];
+        $code = $_GET['code'];
+
+        $select = $con->prepare("SELECT * FROM users WHERE email='$email' and code='$code'");
+        $select->setFetchMode(PDO::FETCH_ASSOC);
+        $select->execute();
+        $data=$select->fetch();
+        
+        echo $data;
+                }
+    }
+
+    catch(PDOException $e)
+    {
+        echo "error".$e->getMessage();
+    }
+    
+    }
+
+?>
